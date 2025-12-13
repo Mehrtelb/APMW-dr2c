7f95be684a40969da05fe23166af7ae8f951e930
Mehrtelb <mehrtelb@tuta.io>
2025-12-13 12:42:34 +0100

    Changed some descriptions

--- Options.py
+++ Options.py
@@ -20,15 +20,17 @@ class Goal(Choice):
 class GoalNumLevels(BaseOptions.Range):
     """
     If 'Specific Modes' or 'Random Modes' are chosen as the goal, this is how many Modes must be beaten.
+    Each Mode takes about 45 minutes (Short Trip is shorter and Long Winding and Marathon are longer respectively
+    but could take a lot longer with Death Link enabled) and adds 3-4 Locations.
     """
     display_name = "Goal: Number of Modes"
     range_start = 1
     range_end = 20
     default = 3
 
-class GoalSpecificLevels(BaseOptions.OptionSet):
+class GoalSpecificModes(BaseOptions.OptionSet):
     """
-    If 'Complete Specific Levels' is chosen as the goal, all levels chosen here must be completed.
+    If 'Specific Modes' is chosen as the goal, all modes chosen here must be beaten to finish the game.
     """
     display_name = "Goal: Specific Modes"
     valid_keys = frozenset({
@@ -74,6 +76,6 @@ class Passport(Toggle):
     Dude, where's my passport? Since Canada is the last beacon of civilization, you will need a passport, I can't
     let you cross the border otherwise mate, sorry aboot that.
     Creates a 'Passport' item that needs to be collected before being able to finish a run.
-    !WARNING EXTREMELY STUPID SETTING, WILL LIKELY SOFTLOCK OR FAIL TO GENERATE FOR NOW!
+    !WARNING EXTREMELY STUPID SETTING, WILL VERY LIKELY CAUSE YOU TO BK UNLESS YOU FORCE THIS TO BE A LOCAL ITEM!
     """
     display_name = "Passport"
