d0a0bd57fe9211a31504abaa805bcff69b98f76f
Mehrtelb <mehrtelb@tuta.io>
2025-12-14 15:53:03 +0100

    item name to id

--- __init__.py
+++ __init__.py
@@ -12,7 +12,13 @@ from BaseClasses import Region, Location, MultiWorld, Item, LocationProgressType
     CollectionState
 from worlds.AutoWorld import World, LogicMixin, WebWorld
 
+game_name = "Death Road to Canada"
 
+class DR2CLocation(AP.Location):
+    game: str = game_name
+
+class DR2CItem(AP.Item):
+    game: str = game_name
 
 class DR2CWeb(WebWorld):
     rich_text_options_doc = True
@@ -34,7 +40,7 @@ class DR2CWeb(WebWorld):
 
     bug_report_page = https://github.com/Mehrtelb/APMW-dr2c/issues/new?assignees=&labels=bug%2C+needs+investigation&template=bug_report.md&title=
 
-class dr2c(World):
+class DR2C(World):
     """Death Road to Canada is a Randomly Generated Road Trip Action-RPG. You have to manage a car full of jerks as they
     explore cities, find weird people, and face up to 500 zombies at once.
     Everything's randomized: locations, events, survivor personalities. There's a different story every time you play.
@@ -54,5 +60,16 @@ class dr2c(World):
     options = DR2COptions
     topology_present = False
     settings: typing.ClassVar[DeathRoadtoCanadaSettings]
+    required_client_version = (0, 6, 4)
 
     web = DR2CWeb()
+
+    item_name_to_id = {name: data.code for name, data in item_table.items()}
+    location_name_to_id = give_all_locations_table()
+
+    def create_item(self, name: str) -> DR2CItem:
+        item_id = self.item_name_to_id[name]
+        item_data = self.item_table[item_id]
+        classification = item_data.classification
+
+        return DR2CItem(name, classification, item_id, self.player)
